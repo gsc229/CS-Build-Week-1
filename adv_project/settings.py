e8fddd5921f857e237513bd2c0461db2233239b9
Rayven <rayven.burns@gmail.com>
2020-03-04 13:02:48 -0500

    another change, hoping this works

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 import django_heroku
