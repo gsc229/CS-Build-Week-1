60332ee9b83d64522edcde103c4b24a4548ac7e4
Greg <gsc229@gmail.com>
2020-03-05 12:03:31 -0500

    pushing to heroku

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -157,4 +157,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 
-del DATABASES['default']['OPTIONS']['sslmode']
+#del DATABASES['default']['OPTIONS']['sslmode']
