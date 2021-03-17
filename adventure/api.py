a031c5d1094a1438b72f3fea9867bff3221536dd
greg <gsc229@gmail.com>
2021-03-17 15:27:03 -0400

    making moves

--- adventure/api.py
+++ adventure/api.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-# from pusher import Pusher
+from pusher import Pusher
 from django.http import JsonResponse
 #from decouple import config
 from django.contrib.auth.models import User
